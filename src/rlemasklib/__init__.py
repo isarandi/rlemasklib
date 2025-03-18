85174f51c8b8034c63aceaf382de6849a53792ed
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2025-03-18 20:39:48 +0100

    Fix docs

--- src/rlemasklib/__init__.py
+++ src/rlemasklib/__init__.py
@@ -10,6 +10,47 @@ There are two ways to use this library:
 
 """
 
+__all__ = [
+    "RLEMask",
+    "encode",
+    "decode",
+    "compress",
+    "decompress",
+    "ones",
+    "zeros",
+    "ones_like",
+    "zeros_like",
+    "full",
+    "empty",
+    "area",
+    "centroid",
+    "intersection",
+    "union",
+    "iou",
+    "difference",
+    "symmetric_difference",
+    "from_bbox",
+    "to_bbox",
+    "from_polygon",
+    "crop",
+    "pad",
+    "connected_components",
+    "largest_connected_component",
+    "remove_small_components",
+    "fill_small_holes",
+    "erode",
+    "dilate",
+    "opening",
+    "closing",
+    "erode2",
+    "dilate2",
+    "opening2",
+    "closing2",
+    "shift",
+    "BoolFunc",
+    "merge",
+]
+
 from rlemasklib.oop import RLEMask
 
 
