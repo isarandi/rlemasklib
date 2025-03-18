bf50645146d9902051e7ac1e2d243b5bbda45bfb
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2025-03-18 20:36:47 +0100

    Fix requirements

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,13 @@ import importlib
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../src'))
+#sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('../src'))
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/rlemasklib')))
+
 
 from conf_spec import project, project_slug, release
 
