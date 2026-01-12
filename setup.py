f8cb91de6d89d2b118b8aeda3d0f22a772177c12
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2026-01-12 03:39:03 +0100

    Apply code formatting to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 import glob
 
 all_c_files = glob.glob("src/rlemasklib/c/*.c")
-single_translation_unit = 'src/rlemasklib/c/single_translation_unit.c'
+single_translation_unit = "src/rlemasklib/c/single_translation_unit.c"
 
 use_single_translation_unit = True
 if use_single_translation_unit:
@@ -16,22 +16,25 @@ else:
 
 # Platform-specific compiler arguments
 # MSVC uses different flag syntax than GCC/Clang
-if sys.platform == 'win32':
-    extra_compile_args = ['/O2', '/wd4505']  # /O2=optimize, /wd4505=disable unused function warning
+if sys.platform == "win32":
+    extra_compile_args = [
+        "/O2",
+        "/wd4505",
+    ]  # /O2=optimize, /wd4505=disable unused function warning
 else:
-    extra_compile_args = ['-Wno-cpp', '-Wno-unused-function', '-std=c99', '-O3']
+    extra_compile_args = ["-Wno-cpp", "-Wno-unused-function", "-std=c99", "-O3"]
 
 ext_modules = [
     Extension(
-        'rlemasklib.rlemasklib_cython',
-        sources=['src/rlemasklib/rlemasklib_cython.pyx'] + c_files,
-        include_dirs=[np.get_include(), 'src/rlemasklib/c'],
+        "rlemasklib.rlemasklib_cython",
+        sources=["src/rlemasklib/rlemasklib_cython.pyx"] + c_files,
+        include_dirs=[np.get_include(), "src/rlemasklib/c"],
         extra_compile_args=extra_compile_args,
     ),
     Extension(
-        'rlemasklib.oop_cython',
-        sources=['src/rlemasklib/oop_cython.pyx'] + c_files,
-        include_dirs=[np.get_include(), 'src/rlemasklib/c'],
+        "rlemasklib.oop_cython",
+        sources=["src/rlemasklib/oop_cython.pyx"] + c_files,
+        include_dirs=[np.get_include(), "src/rlemasklib/c"],
         extra_compile_args=extra_compile_args,
     ),
 ]
